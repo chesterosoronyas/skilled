03bb480a09341a9ab895ac0666bfc242dd377540
chesterosoronyas <conyasetia@gmail.com>
2026-05-04 22:37:55 +0300

    implement homepage and skillcard

---
+++ type.d.ts
@@ -0,0 +1,13 @@
+
+ export interface SkillRecord {
+  id: string;
+  title: string; // Write Code
+  slug: string; // write-code
+  description: string;
+  category: string;
+  tags: string[];
+  installCommand: string;
+  createdAt: string | null;
+  authorClerkId: string | null;
+  authorEmail: string | null;
+}
